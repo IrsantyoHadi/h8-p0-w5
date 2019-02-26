a839d49d3f150a55de402529a8f3f91cc7ecadc6
Irsantyo Hadi <irsantyohadi@gmail.com>
2019-02-26 12:43:44 +0700

    perbaikan kondisi 0

--- exercise-3.js
+++ exercise-3.js
@@ -1,16 +1,19 @@
 function makanTerusRekursif(waktu) {
-    // you can only write your code here!
-    if(waktu <= 15){
-      return 1
-    }else{
-      var dataSisa = waktu - 15
-      return 1 + makanTerusRekursif(dataSisa)
-    }
+  // you can only write your code here!
+  if(waktu <= 0){
+    return 0
   }
-  
-  // TEST CASES
-  console.log(makanTerusRekursif(66)); // 5
-  console.log(makanTerusRekursif(100)); // 7
-  console.log(makanTerusRekursif(90)); // 6
-  console.log(makanTerusRekursif(10)); // 1
-  console.log(makanTerusRekursif(0)); // 0
+  if(waktu <= 15){
+    return 1
+  }else{
+    var dataSisa = waktu - 15
+    return 1 + makanTerusRekursif(dataSisa)
+  }
+}
+
+// TEST CASES
+console.log(makanTerusRekursif(66)); // 5
+console.log(makanTerusRekursif(100)); // 7
+console.log(makanTerusRekursif(90)); // 6
+console.log(makanTerusRekursif(10)); // 1
+console.log(makanTerusRekursif(-15)); // 0
